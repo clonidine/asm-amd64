4a026e8e62c4fa7e93f6606712f972eee3f855ce
clonidine <lcly@duck.com>
2024-01-03 14:25:15 -0300

    creating constants

--- src/fuck.asm
+++ src/fuck.asm
@@ -2,18 +2,22 @@ global _start
 
 section .data
 message: db 'Hello, world!', 10
+msg_len: equ $ - message
+
+SYS_WRITE equ 1
+SYS_EXIT equ 60
 
 section .text
 _start:
     jmp print
     jmp exit
 print:
-    mov rax, 1
+    mov rax, SYS_WRITE
     mov rdi, 1
     mov rsi, message
-    mov rdx, 14
+    mov rdx, msg_len
     syscall
 exit:
-    mov rax, 60
+    mov rax, SYS_EXIT
     xor rdi, rdi
     syscall
